5aa14ef5688b1c6ed2400f9ddbd32fbb8ad5bd2d
Vladimir Kurnikov <kurnikov2109@gmail.com>
2025-06-22 00:59:00 +0300

    Add function sales and cours

--- website/main.js
+++ website/main.js
@@ -1,8 +1,28 @@
 "use strict";
-let sumMarks = 0
+
+var listOfSales = ['TR2025', 'GDE2022', 'TRUEKRISLOH']
+
+/*
+Данная функция-конструкция позваляет удобно создавать объекты курсов
+*/
+
+function Cours (name, about, price){
+  this.name = name;
+  this.about = about;
+  this.price = price;
+}
+
+function sale (cours, gottenTextOfSale){
+    let curretPrice = cours.price, count = 0;
+    for (let arg of listOfSales){
+      if (gottenTextOfSale == arg) {
+        return curretPrice / 2}
+      else count = count + 1
+    if (count == listOfSales.length) return curretPrice
+}}
 
 function sumMarksEge (firstTest, secondTest, thirdTest){
-        sumMarks = firstTest + secondTest + thirdTest
+        let sumMarks = firstTest + secondTest + thirdTest;
         if (sumMarks> 300 || sumMarks <= 0) {
           return 'Pizdets'
         } else {
@@ -10,5 +30,7 @@ function sumMarksEge (firstTest, secondTest, thirdTest){
         }
     };
 
+let coursOfMath = new Cours('Курс по математике', 'Это лучший курс по математике', 2000)
+console.log(sale(coursOfMath, 'GDE2022'))
 
-console.log(sumMarksEge(0, 1, 0));
+
